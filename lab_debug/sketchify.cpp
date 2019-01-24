0e0ee764dac181ac9f58a2f6d302e4d33a29634b
Dingyu Peng <dpeng4@illinois.edu>
2019-01-23 20:45:56 -0600

    submit

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -5,7 +5,7 @@
 
 #include <cstdlib>
 #include <cmath>
-
+#include <iostream>
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
 using namespace cs225;
@@ -29,41 +29,46 @@ PNG* setupOutput(unsigned w, unsigned h) {
  * @return a pointer to the color to use when sketchifying
  */
 HSLAPixel* myFavoriteColor() {
-    HSLAPixel p(-1, 0.8, 0.5);
-    return &p;
+    HSLAPixel *pp = new HSLAPixel(100,1,0.5,1);
+    return pp;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    ///PNG* original = NULL;
+    PNG* original = new PNG();
+    ////
     original->readFromFile(inputFile);
+    ////
     unsigned width = original->width();
     unsigned height = original->height();
 
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
-
+    ////
+    PNG* output = setupOutput(width, height);
+    ////
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor();
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
-            // Calculate the pixel difference
-            HSLAPixel& prev = original->getPixel(x - 1, y - 1);
-            HSLAPixel& curr = original->getPixel(x, y);
-            double diff = std::fabs(curr.h - prev.h);
+    for (unsigned y = 1; y < height; y++) {
+      for (unsigned x = 1; x < width; x++) {
+        // Calculate the pixel difference
+        HSLAPixel& prev = original->getPixel(x - 1, y - 1);
+        HSLAPixel& curr = original->getPixel(x, y);
+        double diff = std::fabs(curr.h - prev.h);
 
-            // If the pixel is an edge pixel,
-            // color the output pixel with my favorite color
-            HSLAPixel currOutPixel = (*output).getPixel(x, y);
-            if (diff > 20) {
-                currOutPixel = *myPixel;
-            }
+        // If the pixel is an edge pixel,
+        // color the output pixel with my favorite color
+        ////
+        HSLAPixel& currOutPixel = (*output).getPixel(x, y);
+        ////
+        if (diff > 20) {
+            currOutPixel = *myPixel;
+            //std::cout<<"*myPixel is "<<(*myPixel)<<std::endl;
         }
+      }
     }
 
     // Save the output file
